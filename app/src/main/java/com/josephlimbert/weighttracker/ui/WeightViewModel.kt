c6515331b6e13b36743219db2977399ce9abf927
Joe <hoodzmtb@gmail.com>
2026-03-06 22:31:13 -0500

    update weight view model

--- app/src/main/java/com/josephlimbert/weighttracker/ui/WeightViewModel.kt
+++ app/src/main/java/com/josephlimbert/weighttracker/ui/WeightViewModel.kt
@@ -1,6 +1,7 @@
 package com.josephlimbert.weighttracker.ui
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import com.josephlimbert.weighttracker.MainViewModel
 import com.josephlimbert.weighttracker.R
 import com.josephlimbert.weighttracker.data.model.ErrorMessage
@@ -11,6 +12,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import javax.inject.Inject
@@ -18,18 +22,87 @@ import javax.inject.Inject
 
 @HiltViewModel
 class WeightViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
     private val authRepository: AuthRepository,
     private val weightItemRepository: WeightItemRepository
 ) : MainViewModel() {
     private val _isLoadingUser = MutableStateFlow(true)
+    private val _weightList = MutableStateFlow<List<Weight>>(emptyList())
     private val _weight = MutableStateFlow<Weight?>(null)
+    private val _currentWeight = MutableStateFlow<Weight?>(null)
+    private val _startingWeight = MutableStateFlow<Weight?>(null)
+    private val _goalWeight = MutableStateFlow<Double?>(null)
+
     val isLoadingUser: StateFlow<Boolean>
         get() = _isLoadingUser.asStateFlow()
 
-    val weightList = weightItemRepository.getWeightList(authRepository.currentUserIdFlow)
+    val weightList: StateFlow<List<Weight>>
+        get() = _weightList.asStateFlow()
+
     val weight: StateFlow<Weight?>
         get() = _weight.asStateFlow()
+
+    val currentWeight: StateFlow<Weight?>
+        get() = _currentWeight.asStateFlow()
+
+    val startingWeight: StateFlow<Weight?>
+        get() = _startingWeight.asStateFlow()
+
+    val goalWeight: StateFlow<Double?>
+        get() = _goalWeight.asStateFlow()
+
+    val totalLossPercent: StateFlow<Double> =
+        combine(_startingWeight, _currentWeight, _goalWeight) { starting, current, goal ->
+            val result = if (starting != null && current != null && goal != null) {
+                (((starting.weight - current.weight) / (starting.weight - goal)) * 100.0).coerceAtMost(
+                    100.0
+                )
+            } else { 0.0 }
+            return@combine result
+        } as StateFlow<Double>
+
+    val totalLossWeight: StateFlow<Double> =
+    combine(_startingWeight, _currentWeight) { starting, current ->
+        val result = if (starting != null && current != null) {
+            starting.weight - current.weight
+        } else { 0.0 }
+        return@combine result
+    } as StateFlow<Double>
+
+    val targetLoss: StateFlow<Double> =
+        combine(_startingWeight, _goalWeight) { starting, goal ->
+        val result = if (starting != null && goal != null) {
+            starting.weight - goal
+        } else { 0.0 }
+        return@combine result
+    } as StateFlow<Double>
+
+    val targetLeft: StateFlow<Double> =
+        combine(_startingWeight, _currentWeight, _goalWeight) { starting, current, goal ->
+        val result = if (starting != null && current != null && goal != null) {
+            (starting.weight - goal) - (starting.weight - current.weight)
+        } else { 0.0 }
+        return@combine result
+    } as StateFlow<Double>
+
+    init {
+        loadWeightList()
+    }
+
+    private fun loadWeightList() {
+        viewModelScope.launch {
+            weightItemRepository.getWeightList(authRepository.currentUserIdFlow)
+                .collect { weights ->
+                    _weightList.value = weights
+                    if (!weights.isEmpty()) {
+                        _startingWeight.value = weights[weights.size - 1]
+                        _currentWeight.value = weights[0]
+                    }
+                }
+            if (authRepository.currentUser != null) {
+                //TODO: add user info to user repo to get goal weight
+            }
+        }
+    }
 
     fun loadCurrentUser() {
         launchCatching {
